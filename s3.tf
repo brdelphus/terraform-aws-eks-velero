3f266ccb6f7cfdbe0476c389db4e923bdf54de81
brdelphus <delphus@delphus.org>
2022-05-30 09:45:57 -0300

    Update s3.tf
    update s3 to acl private

--- s3.tf
+++ s3.tf
@@ -2,9 +2,13 @@ resource "aws_s3_bucket" "velero" {
   count = (var.enabled && var.create_bucket) ? 1 : 0
 
   bucket = var.bucket_name
-  acl    = "private"
+  #acl    = "private"
 
   tags = {
     Name = "EKS Velero"
   }
+}
+resource "aws_s3_bucket_acl" "velero_acl_private" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
